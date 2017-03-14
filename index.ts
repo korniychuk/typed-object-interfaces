f4dc3c2331df1bf76e336a2e7655216154eb5c33
Anton Korniychuk <ancor.root@gmail.com>
2017-03-14 15:25:04 +0200

    First beta version

---
+++ index.ts
@@ -0,0 +1,23 @@
+export interface NumberObject {
+  [name: string]: number;
+}
+
+export interface StringObject {
+  [name: string]: string;
+}
+
+export interface BooleanObject {
+  [name: string]: boolean;
+}
+
+export interface DefaultObject {
+  [name: string]: string | number;
+}
+
+export interface StdObject {
+  [name: string]: string | number | boolean;
+}
+
+export interface AnyObject {
+  [name: string]: any;
+}
